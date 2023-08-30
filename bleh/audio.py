8e9c96ba68a7c8cda25ec828ac3e06c400dd178f
Erik Riiska <erik@Eriks-MacBook-Pro.local>
2023-08-30 15:03:07 -0700

    arduino and fixups

--- bleh/audio.py
+++ bleh/audio.py
@@ -5,10 +5,15 @@ import serial
 
 import pyaudio
 
-chunk = 1024 # Record in chunks of 1024 samples
+def remap(i):
+    if i < 4:
+        return i
+    return 7 - i
+
+chunk = 1024 * 4 # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16
 channels = 2
-fs = 44100  # Record at 44100 samples per second
+fs = 1024 * 16 * 4  # samples per second
 
 p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
@@ -22,7 +27,7 @@ for i in range(0, p.get_device_count()):
 #ToDo change to your device ID
 device_id = 5
 device_info = p.get_device_info_by_index(device_id)
-channels = device_info["maxInputChannels"] if (device_info["maxOutputChannels"] < device_info["maxInputChannels"]) else device_info["maxOutputChannels"]
+# channels = device_info["maxInputChannels"] if (device_info["maxOutputChannels"] < device_info["maxInputChannels"]) else device_info["maxOutputChannels"]
 # https://people.csail.mit.edu/hubert/pyaudio/docs/#pyaudio.Stream.__init__
 with serial.Serial(port='/dev/cu.usbmodem101', baudrate=9600, timeout=10) as ser:
     time.sleep(5)
@@ -34,40 +39,70 @@ with serial.Serial(port='/dev/cu.usbmodem101', baudrate=9600, timeout=10) as ser
                     input_device_index=device_info["index"],
                     )
 
-    # frames = []  # Initialize array to store frames
     base_hue = 0
     print('\nRecording', device_id, '...\n')
-    m = [1] * 5
-    t = time.time()
-    frame_total = [0] * 5
-    frame_num = 0
+    m = 1
+    s = [0.0] * 4
+    l, r = [0.0] * 4, [0.0] * 4
+
     try:
         while True:
+            while not stream.get_read_available():
+                continue
             data = stream.read(chunk)
             data_np = np.frombuffer(data, dtype='h')
-            data_np = np.abs(np.fft.fft(data_np, 5, axis=0))
-            for i, v in enumerate(data_np):
-                frame_total[i] += v
-            frame_num += 1
-            for i, v in enumerate(data_np):
-                m[i] = max(m[i], v)
 
-            if time.time() > t:
-                lst = [ x * 1.0/(m[i]*frame_num) for i, x in enumerate(frame_total)]
-                lst = [colorsys.hsv_to_rgb((base_hue + (i/(30)))%1, 1.0, x) for i, x in enumerate(lst)]
-                print(lst)
-                b = bytearray()
-                for x in lst:
-                    for rgb in x:
-                        b.append(int(30*rgb))
-                for x in lst[::-1]:
-                    for rgb in x:
-                        b.append(int(30*rgb))
+            # data_np = np.abs(np.fft.fft(data_np, axis=0))
+            # d = [
+            #         (np.sum(data_np[0:512]))**2,
+            #         (np.sum(data_np[512:4096]))**2,
+            #         (np.sum(data_np[4096:7770]))**2,
+            #         (np.sum(data_np[7770:]))**2
+            #     ]
+            # m = max(m, np.max(d))
+            # s = [max((s[i] * 0.05 +d[i]*0.95), d[i]) for i in range(4)]
+
+            l_channel = data_np[0::2]
+            r_channel = data_np[1::2]
+            l_channel = np.abs(np.fft.fft(l_channel, axis=0))
+            dl = [
+                    (np.sum(l_channel[0:256]))**2,
+                    (np.sum(l_channel[256:2048]))**2,
+                    (np.sum(l_channel[2048:3850]))**2,
+                    (np.sum(l_channel[3850:]))**2
+                ]
+            m = max(m, np.max(dl))
+            l = [max((l[i] * 0.05 + dl[i]*0.95), dl[i]) for i in range(4)]
+
+            r_channel = np.abs(np.fft.fft(r_channel, axis=0))
+            dr = [
+                    (np.sum(r_channel[0:256]))**2,
+                    (np.sum(r_channel[256:2048]))**2,
+                    (np.sum(r_channel[2048:3850]))**2,
+                    (np.sum(r_channel[3850:]))**2
+                ]
+            m = max(m, np.max(dr))
+            r = [max((r[i] * 0.05 + dr[i]*0.95), dr[i]) for i in range(4)]
+            # print(len(r_channel))
+            
+            # for i in range(4)[::-1]:
+            #     l[i] = max(l[i] * 0.5, (max(l_channel[i], l_channel[i] * 0.5 + l[i] * 0.5)) - sum(l[i:]))
+            #     r[i] = max(r[i] * 0.5, (max(r_channel[i], r_channel[i] * 0.5 + r[i] * 0.5)) - sum(r[i:]))
+            # m = max(m, np.max(l), np.max(r))
+    
+
+            # l = [colorsys.hsv_to_rgb((base_hue + (i/(15)))%1, 1.0, x*1.0/m) for i, x in enumerate(l)]
+            # r = [colorsys.hsv_to_rgb((base_hue + (i/(15)))%1, 1.0, x*1.0/m) for i, x in enumerate(r)]
+            b = bytearray()
+            for x in [colorsys.hsv_to_rgb((base_hue + (i/(15)))%1, 1.0, x*1.0/m) for i, x in enumerate(r[0:4])]:
+                for rgb in x:
+                    b.append(int(64*rgb))
+            for x in [colorsys.hsv_to_rgb((base_hue + 0.5 + (i/(15)))%1, 1.0, x*1.0/m) for i, x in enumerate(l[0:4])][::-1]:
+                for rgb in x:
+                    b.append(int(64*rgb))
+            if ser.writable():
                 ser.write(b)
-                t = time.time() + (1.0/30)
-                frame_num = 0
-                frame_total = [0] * 5
-                base_hue += 1.0/250
+            base_hue += 1.0/500
     finally:
         # Stop and close the stream 
         stream.stop_stream()
---
+++ bleh/listen_and_light/listen_and_light.ino
@@ -0,0 +1,52 @@
+#include "FastLED.h"
+
+#define NUM_LEDS 8
+#define DATA_PIN 3
+
+CRGBArray<NUM_LEDS> leds;
+CRGBArray<3> colors;
+
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+  FastLED.addLeds<PL9823, DATA_PIN, RGB>(leds, NUM_LEDS);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] =  CRGB(60,60,60);
+  }
+  FastLED.show();
+  delay(1000);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] =  CRGB(0,0,0);
+  }
+  FastLED.show();
+}
+
+static uint8_t hue;
+
+int d = 80;
+
+struct Frame {
+  int duration_ms;
+  CRGB image[NUM_LEDS];
+};
+
+char buf[3 * NUM_LEDS] = {};
+
+char x;
+
+void loop(){
+  while (Serial.available() < 3 * NUM_LEDS) {}
+  Serial.println("YOOOO!");
+  x = Serial.readBytes(buf, 3 * NUM_LEDS);
+  Serial.println(buf);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i].red = buf[3*i];
+    leds[i].green = buf[3*i + 1];
+    leds[i].blue = buf[3*i + 2];
+  }
+  FastLED.show();
+}
+
+
+
+
